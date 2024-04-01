cec55100a9cd279c3786ca3d1941588b2d84c385
giovanna-britto <giovanna.vieira@sou.inteli.edu.br>
2024-04-01 17:18:02 -0300

    Adição do Código para aumentar a autoestima da pessoa

---
+++ pensamentoComputacional.js
@@ -0,0 +1,15 @@
+const read = require('readline-sync');
+
+function aumentarEstima(nome, quantidade){
+    for(let i = 0; i < lerQuantidade; i++){
+        console.log(lerNome + " é a pessoa mais inteligente que já passou por aqui");
+    }
+}
+
+let lerNome = read.question("Qual o seu nome?");
+let lerQuantidade = parseInt(read.question("Você precisa aumentar sua autoestima quantas vezes?"));
+
+aumentarEstima(lerNome, lerQuantidade);
+
+
+
